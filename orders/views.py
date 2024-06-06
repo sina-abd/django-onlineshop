827e4c4c25d7de426ed0e6f6985bd026b65d9015
Sina <sinaedu7@gmail.com>
2024-06-06 23:14:10 +0330

    added zarinpal code(couldn't test it cause didn't have a merchant number

--- orders/views.py
+++ orders/views.py
@@ -5,6 +5,11 @@ from home.models import Product
 from .forms import CartAddForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Order, OrderItem
+from django.conf import settings
+from dotenv import load_dotenv
+import os
+import json
+import requests
 
 
 class CartView(View):
@@ -42,3 +47,61 @@ class OrderCreateView(LoginRequiredMixin, View):
             OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
         cart.clear()
         return redirect('orders:order_detail', order.id)
+    
+description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"
+CallbackURL = 'http://127.0.0.1:8080/orders/verify/'
+    
+class OrderPayView(LoginRequiredMixin, View):
+    def get(self, request, order_id):
+        order = Order.objects.get(id=order_id)
+        request.session['order_pay'] = {
+            'order_id': order_id
+        }
+        load_dotenv()
+        data = {
+        "MerchantID": os.get_env('MERCHANT'),
+        "Amount": order.get_total_price(),
+        "Description": description,
+        "Phone": request.user.phone_number,
+        "CallbackURL": CallbackURL,
+    }
+        data = json.dumps(data)
+        headers = {'content-type': 'application/json', 'content-length': str(len(data)) }
+        try:
+            response = requests.post(os.getenv("ZP_API_REQUEST"), data=data,headers=headers, timeout=10)
+
+            if response.status_code == 200:
+                response = response.json()
+                if response['Status'] == 100:
+                    return {'status': True, 'url': os.getenv("ZP_API_REQUEST") + str(response['Authority']), 'authority': response['Authority']}
+                else:
+                    return {'status': False, 'code': str(response['Status'])}
+            return response
+        except requests.exceptions.Timeout:
+            return {'status': False, 'code': 'timeout'}
+        except requests.exceptions.ConnectionError:
+            return {'status': False, 'code': 'connection error'}
+
+class OrderVerifyView(LoginRequiredMixin ,View):
+    def get(self, request):
+        order_id = request.session['order_pay']['order_id']
+        order = Order.objects.get(id=int(order_id))
+        data = {
+        "MerchantID": os.getenv("MERCHANT"),
+        "Amount": order.get_total_price,
+        "Authority": request.GET['authority'],
+        }
+        data = json.dumps(data)
+        headers = {'content-type': 'application/json', 'content-length': str(len(data)) }
+        response = requests.post(os.getenv("ZP_API_VERIFY"), data=data,headers=headers)
+
+        if response.status_code == 200:
+            response = response.json()
+            if response['Status'] == 100:
+                order.paid = True
+                order.save()
+                return {'status': True, 'RefID': response['RefID']}
+            
+            else:
+                return {'status': False, 'code': str(response['Status'])}
+        return response
