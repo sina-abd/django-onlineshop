0432d608fdd099b64ea083d03c4396247bf965f8
Sina <sinaedu7@gmail.com>
2024-06-09 17:48:09 +0330

    added permissions:

--- accounts/admin.py
+++ accounts/admin.py
@@ -13,7 +13,7 @@ class UserAdmin(BaseUserAdmin):
 
     fieldsets = (
         (None, {"fields": ('email', 'phone_number', 'full_name', 'password')}),
-        ('permissions', {'fields': ('is_active', 'is_admin', 'last_login')}),
+        ('permissions', {'fields': ('is_active', 'is_admin', 'is_superuser', 'last_login', 'groups', 'user_permissions')}),
     )
 
     add_fieldsets = (
@@ -22,9 +22,8 @@ class UserAdmin(BaseUserAdmin):
     
     search_fields = ('email', 'full_name')
     ordering = ('full_name',)
-    filter_horizontal = ()
+    filter_horizontal = ('groups', 'user_permissions')
 
-admin.site.unregister(Group)
 admin.site.register(User, UserAdmin)
 
 @admin.register(OTPCode)
