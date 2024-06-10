68f9ca65ab392a46034cc3ddf60d1b8f849b7b53
Sina <sinaedu7@gmail.com>
2024-06-10 15:28:46 +0330

    changed the database to postgresql

--- A/settings.py
+++ A/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'A.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangoshop',
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
