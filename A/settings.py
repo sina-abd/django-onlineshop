aa15f74b518990d6a833fdaf79c81a07363436a1
Sina <sinaedu7@gmail.com>
2024-06-12 20:14:09 +0330

    added caching using redis

--- A/settings.py
+++ A/settings.py
@@ -95,6 +95,15 @@ DATABASES = {
     }
 }
 
+CASHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',
+    }
+}
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
